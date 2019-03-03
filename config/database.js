8c486257e028f64b54d8a3c31ab822bb58cea752
Akash Agarwal <akash.ka01@gmail.com>
2019-03-03 16:58:31 +0530

    connect to DB server

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	// url: "mongodb://heroku_7bl0f9cp:c765avlu1acm9pvbu1t56mnmfa@ds163053.mlab.com:63053/heroku_7bl0f9cp"
-	url: "mongodb://localhost:27017/slate"
+	url: "mongodb://admin:Abcd123$0@ds159025.mlab.com:59025/heroku_xlbk8340"
+	// url: "mongodb://localhost:27017/slate"
 }
