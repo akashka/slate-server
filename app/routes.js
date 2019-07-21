b9646ec8ad4453ec2375254cedff1f9f274cdc5e
subham jain <subhamjain406@gmail.com>
2019-07-21 14:46:33 +0530

    added phase 2 controller

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,11 @@ var AuthenticationController = require('./controllers/authentication'),
     FranchiseController = require('./controllers/franchisees'),
     InstructorController = require('./controllers/instructors'),
     StudentController = require('./controllers/students'),
+    NotificationController = require('./controllers/notifications'),
+    PostOrderController = require('./controllers/post-order'),
+    PriceMappingController = require('./controllers//price-mapping'),
+    InventoryController = require('./controllers/inventory'),
+    PassBookControler = require('./controllers/passbook'),
     express = require('express'),
     passportService = require('../config/passport'),
     passport = require('passport');
@@ -26,7 +31,12 @@ module.exports = function (app) {
         centerRoutes = express.Router(),
         franchiseRoutes = express.Router(),
         instructorRoutes = express.Router(),
-        studentRoutes = express.Router();
+        studentRoutes = express.Router(),
+        notificationsRoutes = express.Router(),
+        postOrderRoutes = express.Router(),
+        priceMappingRoutes = express.Router(),
+        inventoryRoutes = express.Router(),
+        passbookRoutes = express.Router();
 
     apiRoutes.use('/auth', authRoutes);
     authRoutes.post('/register', AuthenticationController.register);
@@ -68,6 +78,36 @@ module.exports = function (app) {
     studentRoutes.get('/', StudentController.getStudent);
     studentRoutes.post('/', StudentController.createStudent);
     studentRoutes.put('/', StudentController.updateStudent);
+
+    // Notifications Routes
+    apiRoutes.use('/notifications', notificationsRoutes);
+    notificationsRoutes.get('/', NotificationController.getNotification);
+    notificationsRoutes.post('/', NotificationController.createNotification);
+    notificationsRoutes.put('/', NotificationController.updateNotification);
+
+    // post-order Routes
+    apiRoutes.use('/postorder', postOrderRoutes);
+    postOrderRoutes.get('/', PostOrderController.getPostOrder);
+    postOrderRoutes.post('/', PostOrderController.createPostOrder);
+    postOrderRoutes.put('/', PostOrderController.updatePostOrder);
+
+    // price-mapping Routes
+    apiRoutes.use('/pricemapping', priceMappingRoutes);
+    priceMappingRoutes.get('/', PriceMappingController.getPriceMapping);
+    priceMappingRoutes.post('/', PriceMappingController.createPriceMapping);
+    priceMappingRoutes.put('/', PriceMappingController.updatePriceMapping);
+
+    // inventoty routes
+    apiRoutes.use('/inventory', inventoryRoutes);
+    inventoryRoutes.get('/', InventoryController.getInventory);
+    inventoryRoutes.post('/', InventoryController.createInventory);
+    inventoryRoutes.put('/', InventoryController.updateInventory);
+
+    // passbook routes
+    apiRoutes.use('/passbook', passbookRoutes);
+    passbookRoutes.get('/', PassBookControler.getPassBook);
+    passbookRoutes.post('/', PassBookControler.createPassBook);
+    passbookRoutes.put('/', PassBookControler.updatePassBook);
 
     // Set up routes
     app.use('/api', apiRoutes);
