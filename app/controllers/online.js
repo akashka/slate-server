e41969778b57e7cb0512a74b83520a346e5cd1e1
Akash Agarwal <akash.ka01@gmail.com>
2020-06-07 10:47:42 +0530

    add online classes

---
+++ app/controllers/online.js
@@ -0,0 +1,34 @@
+var Online = require('../models/online');
+
+exports.getOnline = function (req, res, next) {
+    console.log('---------------------------------------------------------');
+    console.log('---------------------------------------------------------');
+    console.log('---------------------------------------------------------');
+    console.log(req);
+    console.log('---------------------------------------------------------');
+    console.log('---------------------------------------------------------');
+    console.log('---------------------------------------------------------');
+    Online.find(function (err, onlines) {
+        if (err) { res.send(err); }
+        res.json(onlines);
+    });
+}
+
+exports.createOnline = function (req, res, next) {
+    let online = req.body;
+    Online.create(online, function (err, online) {
+        if (err) { res.send(err); }
+        res.json(online);
+    });
+}
+
+exports.updateOnline = function (req, res, next) {
+    let id = req.body._id;
+    let online = req.body;
+    delete online._id;
+
+    Online.findOneAndUpdate({ _id: id }, online, { upsert: true, new: true }, function (err, online) {
+        if (err) return res.send(err);
+        res.json(online);
+    });
+}
---
+++ app/models/online.js
@@ -0,0 +1,87 @@
+var mongoose = require("mongoose");
+
+var OnlineSchema = new mongoose.Schema(
+  {
+    txtsname: {
+      type: String
+    },
+    txtsdob: {
+      type: String
+    },
+    txtsadd: {
+      type: String
+    },
+    txtscontact: {
+      type: String
+    },
+    txtlanguage: {
+      type: String
+    },
+    txtinterest: {
+      type: String
+    },
+    txtschool: {
+      type: String
+    },
+    txtschooladd: {
+      type: String
+    },
+    txtfname: {
+      type: String
+    },
+    txtfoccupation: {
+      type: String
+    },
+    txtfoffadd: {
+      type: String
+    },
+    txtfoffcontact: {
+      type: String
+    },
+    txtfmail: {
+      type: String
+    },
+    txtmname: {
+      type: String
+    },
+    txtmoccupation: {
+      type: String
+    },
+    txtmoffadd: {
+      type: String
+    },
+    txtmoffcontact: {
+      type: String
+    },
+    txtmmail: {
+      type: String
+    },
+    txtprograms: {
+      type: Array
+    },
+    txthdyk: {
+      type: String
+    },
+    photo: {
+      type: String
+    },
+    idproof: {
+      type: String
+    },
+    referralcode: {
+      type: String
+    },
+    otp: {
+      type: String
+    },
+    otpvalidated: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Online", OnlineSchema);
