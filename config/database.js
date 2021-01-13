765e2a63f59fb3652ba0875fb81679fea4b3580d
Akash Agarwal <30892454+akashka@users.noreply.github.com>
2021-01-13 11:52:55 +0530

    db update
    db update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	url: "mongodb://admin:Abcd123$0@ds159025.mlab.com:59025/heroku_xlbk8340"
+	url: "mongodb://admin:abcd1234@cluster0-shard-00-00.cjv0p.mongodb.net:27017,cluster0-shard-00-01.cjv0p.mongodb.net:27017,cluster0-shard-00-02.cjv0p.mongodb.net:27017/alohaindia?ssl=true&replicaSet=atlas-xm4jo5-shard-0&authSource=admin&retryWrites=true&w=majority"
+// 	url: "mongodb://admin:Abcd123$0@ds159025.mlab.com:59025/heroku_xlbk8340"
 	// url: "mongodb://localhost:27017/slate"
 }
